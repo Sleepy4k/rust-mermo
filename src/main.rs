374876434bf6625897bb7212e640e1c3321ad96f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-02 22:40:37 +0700

    chore: main file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use std::env;
 use sqlx::PgPool;
 use dotenv::dotenv;
+use tide::{http::headers::HeaderValue, security::{CorsMiddleware, Origin}};
+
 use mermo::{path, user_token};
-use tide::http::headers::HeaderValue;
-use tide::security::{CorsMiddleware, Origin};
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     dotenv().ok();
 
-    let db_url = std::env::var("DATABASE_URL").expect("DB config error");
+    let db_url = env::var("DATABASE_URL").expect("DB config error");
 
     let pool = PgPool::connect(&db_url).await.expect("DB Connection error");
 
@@ -23,8 +24,8 @@ async fn main() -> tide::Result<()> {
     let mut app = tide::with_state(pool.clone());
 
     app.with(cors);
-    // app.with(user_token);
-    
+    app.with(user_token);
+
     path(&mut app).await;
 
     app.listen("0.0.0.0:7004").await?;
